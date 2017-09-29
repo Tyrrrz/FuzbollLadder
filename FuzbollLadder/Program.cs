c9e08b11e59cba08a4ca0c08095a3a6eed6c0df6
Oleksii Holub <oleksii.holub@3shape.com>
2017-09-29 12:18:56 +0300

    Add app version meta tag

--- FuzbollLadder/Program.cs
+++ FuzbollLadder/Program.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace FuzbollLadder
 {
     public class Program
     {
+        public static Version Version { get; private set; }
+
         public static void Main(string[] args)
         {
+            Version = Assembly.GetEntryAssembly().GetName().Version;
             BuildWebHost(args).Run();
         }
 
